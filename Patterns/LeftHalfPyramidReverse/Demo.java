429a0ad9b07f1ff8fa825e91f9c7aa1370721752
Adesh Kanthali <92244882+Adeshkanthali@users.noreply.github.com>
2024-12-04 22:20:04 +0530

    Add files via upload

---
+++ Patterns/LeftHalfPyramidReverse/Demo.java
@@ -0,0 +1,35 @@
+package GithunDSA.Patterns.LeftHalfPyramidReverse;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i=1; i<=n; i++){
+
+            for (int j=1; j<=i; j++){
+                System.out.print("  ");   // two spaces
+            }
+
+            for (int j=n; j>=i; j--){
+                System.out.print(" *");   // one space after star
+            }
+
+            System.out.println();
+        }
+
+    }
+}
+
+/*
+OUTPUT :-
+
+*  *  *  *  *
+   *  *  *  *
+      *  *  *
+         *  *
+            *
+
+*/
+
